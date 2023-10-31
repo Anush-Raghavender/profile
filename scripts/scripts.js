e88b57d97b661a69f376266e12319337e92fcf8d
Anush-Raghavender <64842234+Anush-Raghavender@users.noreply.github.com>
2023-10-31 11:53:16 -0600

    Home Page
    About Section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,3 +5,27 @@ setTimeout(() => {
 
 //Calculate Current Year Dynamically
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+//Display About Content
+document.getElementById("About-Link").onmousedown = function () {
+  document.getElementById("introduction").style.display = "none";
+  document.getElementById("about").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+};
+
+//Display Projects Content
+document.getElementById("Projects-Link").onmousedown = function () {
+  document.getElementById("introduction").style.display = "none";
+  document.getElementById("about").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+};
+
+//Display Skills Content
+document.getElementById("Skills-Link").onmousedown = function () {
+  document.getElementById("introduction").style.display = "none";
+  document.getElementById("about").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+};
